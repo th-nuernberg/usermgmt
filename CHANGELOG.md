c7a5cd6c923227d833f1fe7a55a931aa566ca30a
Dominik Wagner <dominik.wagner@th-nuernberg.de>
2022-10-31 15:40:28 +0100

    remove broken cross compilation tools

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -46,3 +46,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [0.3.7] - 2022-10-31
 ### Added
 - Add cross-compilation tools
+
+## [0.3.8] - 2022-10-31
+### Removed
+- Cross-compilation tools
--- src/cli.rs
+++ src/cli.rs
@@ -4,7 +4,7 @@ pub mod cli {
 
     /// Add, delete, or modify users in LDAP and Slurm simultaneously
     #[derive(Parser, Debug)]
-    #[clap(author = "Author: Dominik Wagner", version = "0.3.7", 
+    #[clap(author = "Author: Dominik Wagner", version = "0.3.8", 
             about = "Simultaneous user management for Slurm and LDAP", long_about = None)]
     pub struct Args {
         /// Operation to conduct on the user. Either add, delete or modify.
