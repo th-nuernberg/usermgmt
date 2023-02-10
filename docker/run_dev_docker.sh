38e098ac2c34fd204b5f134b41d4b6c0c4efd887
BoolPurist <fnaumann@posteo.de>
2023-02-10 13:00:07 +0100

    Fix: made qos multi value

--- docker/run_dev_docker.sh
+++ docker/run_dev_docker.sh
@@ -2,7 +2,9 @@
 
 # A docker compose file might be needed if more than LDAP instance is required
 docker run \
+  -p 389:389 -p 636:636 \
   --volume ./bootstrap_lidfs:/container/service/slapd/assets/config/bootstrap/ldif/custom \
+  --detach \
   osixia/openldap:1.5.0 --copy-service  
   
 
