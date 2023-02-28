d90c423947d2fe38b4760a393df5550589f79c25
BoolPurist <fnaumann@posteo.de>
2023-02-28 09:26:38 +0100

    Ldap: transformed docker run command to docker compose file

--- docker/run_dev_docker.sh
+++ docker/run_dev_docker.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# A docker compose file might be needed if more than LDAP instance is required
-docker run \
-  -p 389:389 -p 636:636 \
-  --volume ./bootstrap_lidfs:/container/service/slapd/assets/config/bootstrap/ldif/custom \
-  --detach \
-  osixia/openldap:1.5.0 --copy-service  
-  
-
+echo "Strating ldap docker container with name ldap."
+docker compose up --detach
 
