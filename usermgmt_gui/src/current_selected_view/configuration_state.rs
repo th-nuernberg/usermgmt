5b895a45e67e94e98ac963ee448fba44d78c0e28
BoolPurist <fnaumann@posteo.de>
2023-11-04 08:00:27 +0100

    Egui: abstracted sorting and labeling config fields

--- usermgmt_gui/src/current_selected_view/configuration_state.rs
+++ usermgmt_gui/src/current_selected_view/configuration_state.rs
@@ -1,9 +1,10 @@
 use usermgmt_lib::config::LoadedMgmtConfig;
 
-use crate::io_resource_manager::IoResourceManager;
+use crate::{drawing::configuration::CacheForConfFiels, io_resource_manager::IoResourceManager};
 
 #[derive(Debug, Default)]
 pub struct ConfigurationState {
+    pub gui_field_cache: CacheForConfFiels,
     pub io_conf: IoResourceManager<LoadedMgmtConfig>,
     pub io_save_conf: IoResourceManager,
 }
